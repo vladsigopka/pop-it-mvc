790bd0642f98c6e6f20f2b6cbca9c69947b25b05
vladsigopka <vlad.tereshhenko.00@mail.ru>
2023-04-13 13:31:46 +0700

    2 commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vladsigopka/www',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a3a304759240e9215c77bcae9b533a5ea48d918c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vladsigopka/www' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a3a304759240e9215c77bcae9b533a5ea48d918c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
